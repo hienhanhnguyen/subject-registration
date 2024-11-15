0e05f118c91b709772c5a66e776f2e04e0470074
NhanHoThanh <hothanhnhanhot@gmail.com>
2024-11-15 19:01:29 +0700

    add runtime js file for server

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "vite-app",
+            script: "npm",
+            args: "run dev",
+            watch: true,
+        },
+    ],
+};
